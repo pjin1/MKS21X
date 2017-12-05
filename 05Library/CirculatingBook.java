bd7ae99096bed802d47a1e39006ed81cfe10127a
pjin1 <30643269+pjin1@users.noreply.github.com>
2017-12-05 09:14:23 -0500

    Create CirculatingBook.java

---
+++ 05Library/CirculatingBook.java
@@ -0,0 +1,43 @@
+public class CirculatingBook extends LibraryBook{
+    private String currentHolder;
+    private String dueDate;
+
+    public CirculatingBook(String author, String title, String ISBN, String callNumber){
+    		super(author, title, ISBN, callNumber);
+    		returned();
+    }
+    
+	public String getCurrentHolder(){
+		return currentHolder;
+	}
+    
+    public String getDueDate(){
+    		return dueDate;
+    }
+
+    public void checkout(String patron, String due){
+    		currentHolder = patron;
+    		dueDate = due;
+    }
+
+    public void returned(){
+    		currentHolder = null;
+    		dueDate = null;
+    }
+
+    public String circulationStatus(){
+    		if (currentHolder != null){
+    			return currentHolder + ": " + dueDate;
+    		}
+    		
+    		return "book available on shelves";
+    }
+
+    public String toString(){
+    		if (currentHolder != null){
+    			return super.toString() + " " + currentHolder + ": " + dueDate;
+    		}
+    		
+    		return super.toString();
+    }
+}
