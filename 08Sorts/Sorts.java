434b3b8d9d589dfb3163040a132b709796356e60
Peter Jin <peterjin@Peters-MacBook-Pro-2.local>
2017-12-10 03:01:21 -0500

    initial commit

---
+++ 08Sorts/Sorts.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+public class Sorts{
+
+  public static String name(){
+    return "10.Jin.Peter";
+  }
+
+  public static boolean isSorted(int[] ary){
+    for(int i = 0; i < ary.length - 1 ; i++){
+      if(ary[i] > ary[i+1]){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private static void swap(int[] ary,int a, int b){
+	int c =ary[a];
+	ary[a] = ary[b];
+	ary[b] = c;
+  }
+
+  //Sorts
+  public static void bogoSort(int[] ary){
+    while(!isSorted(ary)){
+      for(int i = 0 ; i < ary.length; i++){
+        int temp = ary[i];
+        int newSpot = (int)(Math.random()*ary.length);
+        ary[i] = ary[newSpot];
+        ary[newSpot] = temp;
+      }
+    }
+  }
+
+  public static void selectionSort(int[] ary){
+    for (int i =0; i < ary.length; i++){
+      int min = ary[i];
+      int index = i;
+      for (int j = i; j < ary.length; j++){
+        if (ary[j] < min){
+          min = ary[j];
+          index = j;
+        }
+      }
+      swap(ary,i,index);
+    }
+  }
+  
+}
