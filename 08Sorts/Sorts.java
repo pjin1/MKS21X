629dff7c312ae54635fbb08d56f870f5546a23a0
Peter Jin <peterjin@Peters-MacBook-Pro-2.local>
2017-12-11 00:18:43 -0500

    fix

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -1,12 +1,12 @@
 import java.util.Arrays;
 
-public class Sorts{
+public class Sorts {
 
-  public static String name(){
-    return "10.Jin.Peter";
+  public static String name() {
+    return "10,Jin,Peter";
   }
 
-  public static boolean isSorted(int[] ary){
+  public static boolean isSorted(int[] ary) {
     for(int i = 0; i < ary.length - 1 ; i++){
       if(ary[i] > ary[i+1]){
         return false;
@@ -15,14 +15,14 @@ public class Sorts{
     return true;
   }
 
-  private static void swap(int[] ary,int a, int b){
+  private static void swap(int[] ary,int a, int b) {
 	int c =ary[a];
 	ary[a] = ary[b];
 	ary[b] = c;
   }
 
   //Sorts
-  public static void bogoSort(int[] ary){
+  public static void bogoSort(int[] ary) {
     while(!isSorted(ary)){
       for(int i = 0 ; i < ary.length; i++){
         int temp = ary[i];
@@ -33,8 +33,8 @@ public class Sorts{
     }
   }
 
-  public static void selectionSort(int[] ary){
-    for (int i =0; i < ary.length; i++){
+  public static void selectionSort(int[] ary) {
+    for (int i = 0; i < ary.length; i++){
       int min = ary[i];
       int index = i;
       for (int j = i; j < ary.length; j++){
@@ -45,6 +45,29 @@ public class Sorts{
       }
       swap(ary,i,index);
     }
+  }
+  
+  public static void insertionSort(int[] ary) {
+	  for (int i = 1; i < ary.length; i++) {
+		  int temp = ary[i];
+		  int index =i;
+		  while (index > 0 && temp < ary[index-1]) {
+			  ary[index] = ary[index-1];
+			  index--;
+		  }
+		  ary[index] = temp;
+	  }
+  }
+
+  public static void bubbleSort(int[] ary) {
+	  for (int i = 0; i < ary.length; i++) {
+		  for (int j = 1; j < ary.length - i; i++) {
+			  if (ary[j - 1] > ary[j]){
+				  int temp = ary[i-1];
+				  int index = i;
+			  }    
+		  }
+	  }
   }
   
 }
