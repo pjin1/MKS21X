9b1e079151733165eb505075291575997f05e272
Peter Jin <peterjin@Peters-MacBook-Pro-2.local>
2017-12-14 09:03:53 -0500

    update

--- 09GUI/temperatureWindow.java
+++ 09GUI/temperatureWindow.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-public class temp extends JFrame {
+public class temperatureWindow extends JFrame implements ActionListener {
     private Container pane;
 
     private JButton FtoC;
@@ -10,36 +10,47 @@ public class temp extends JFrame {
     private JCheckBox c;
  
     //CONSTRUCTOR SETS EVERYTHING UP
-    public Window2() {
-    this.setTitle("temp converter");
+    public temperatureWindow() {
+    this.setTitle("Temperature Converter");
     this.setSize(600,400);
     this.setLocation(100,100);
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     
     pane = this.getContentPane();
-    pane.setLayout(new FlowLayout());  //NOTE!! THIS CAN BE CHANGED (see below)
+    pane.setLayout(new FlowLayout());
     
     c = new JButton("f to c");
     f = new JButton("c to f");
-    l = new JLabel("temp",null,JLabel.CENTER);
-    t = new JTextField(12);
+    l = new JLabel("Temperature Converter",null,JLabel.CENTER);
+    t = new JTextField(100);
 
-    pane.add(l);
-    pane.add(b);
-    pane.add(t);
     pane.add(c);
+    pane.add(f);
+    pane.add(l);
+    pane.add(t);
     }
-
-
     
     public class Convert{
-    public static double CtoF(double t){
-        return t * 9.0 / 5.0 + 32;
-    }
+	    public static double CtoF(double t){
+	        return t * 9.0 / 5.0 + 32;
+	    }
 
-    public static double FtoC(double t){
-        return (t - 32) * 5.0 / 9.0 ;
-    }
+	    public static double FtoC(double t){
+	        return (t - 32) * 5.0 / 9.0 ;
+	    }
 
     }
+    
+    public void actionPerformed(ActionEvent e){
+		String s = e.getActionCommand();
+		System.out.println(s);
+
+		if (s.equals("Celcius to Fahrenheit")){
+			    
+		}
+		if (s.equals("Fahrenheit to Celsius")){
+		
+		}
+	}
+
 }
