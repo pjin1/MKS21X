9d7f83bbe51ad43e1bec5e109ec900f2ee57c800
Peter Jin <peterjin@Peters-MacBook-Pro-2.local>
2017-12-14 22:36:10 -0500

    edit

--- 09GUI/temperatureWindow.java
+++ 09GUI/temperatureWindow.java
@@ -1,33 +1,39 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+
 public class temperatureWindow extends JFrame implements ActionListener {
     private Container pane;
 
     private JButton FtoC;
     private JButton CtoF;
-    private JLabel l;
+
     private JTextField t;
-    private JCheckBox c;
+
  
     //CONSTRUCTOR SETS EVERYTHING UP
     public temperatureWindow() {
-    this.setTitle("Temperature Converter");
-    this.setSize(600,400);
-    this.setLocation(100,100);
-    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    
-    pane = this.getContentPane();
-    pane.setLayout(new FlowLayout());
-    
-    c = new JButton("f to c");
-    f = new JButton("c to f");
-    l = new JLabel("Temperature Converter",null,JLabel.CENTER);
-    t = new JTextField(100);
-
-    pane.add(c);
-    pane.add(f);
-    pane.add(l);
-    pane.add(t);
+    		this.setTitle("Temperature Converter");
+    		this.setSize(600,400);
+    		this.setLocation(100,100);
+    		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	    
+	    pane = this.getContentPane();
+	    pane.setLayout(new FlowLayout());
+	    
+	    FtoC = new JButton("Fahrenheit to Celcius");
+	    CtoF = new JButton("Celcius to Fahrenheit");
+	    
+	    t = new JTextField(100);
+	
+	    t.addActionListener(this);
+		FtoC.addActionListener(this);
+		CtoF.addActionListener(this);
+		
+		pane.add(t);
+		pane.add(FtoC);
+		pane.add(CtoF);
+	    }
     }
     
     public class Convert{
@@ -35,7 +41,7 @@ public class temperatureWindow extends JFrame implements ActionListener {
 	        return t * 9.0 / 5.0 + 32;
 	    }
 
-	    public static double FtoC(double t){
+	    public static double FtoC(double t){ 
 	        return (t - 32) * 5.0 / 9.0 ;
 	    }
 
@@ -44,13 +50,24 @@ public class temperatureWindow extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e){
 		String s = e.getActionCommand();
 		System.out.println(s);
-
-		if (s.equals("Celcius to Fahrenheit")){
-			    
-		}
-		if (s.equals("Fahrenheit to Celsius")){
 		
+		try {
+			double temperature = Double.parseDouble(t.getText());
+			
+			if (s.equals("Celsius to Fahrenheit")){
+				t.setText("" + (temperature * (9.0 / 5.0 ) + 32));
+			}
+			
+			if (s.equals("Fahrenheit to Celsius")){
+				t.setText("" + ((temperature - 32) * (5.0 / 9.0)));
+			}
 		}
-	}
+		catch (NumberFormatException exc) {
+		    t.setText(">:( insert number");
+		}
+    }
 
 }
+
+
+ 
