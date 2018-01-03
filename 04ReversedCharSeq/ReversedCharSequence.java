5b7728e8093c16aa60c16ae7319f3dd8a70ea65d
Peter Jin <peterjin@Peters-MacBook-Pro-2.local>
2018-01-02 23:38:25 -0500

    fix

--- 04ReversedCharSeq/ReversedCharSequence.java
+++ 04ReversedCharSeq/ReversedCharSequence.java
@@ -1,49 +1,30 @@
 public class ReversedCharSequence implements CharSequence {
     private String seq;
-    private String newSeq;
-		
+	private String output;
+    
     public ReversedCharSequence(String seq) {
     		this.seq = seq;
-    		newSeq = "";
-    		for (int i = seq.length() - 1; i > -1; i--) {
-    			newSeq += seq.substring(i, i+1);
+    		output = "";
+    		for (int i = seq.length() - 1; i >= 0; i--) {
+    			output += seq.substring(i, i+1);
     		}
     }
 
     public char charAt (int index) {
-    		return newSeq.charAt(index);
+    		return output.charAt(index);
     }
 
     public int length() {
-    		return newSeq.length();
+    		return output.length();
     }
 
-    public ReversedCharSequence subSequence (int start, int end) {
-    		String sub ="";
-		for (int i = start; i < end; i++) {
-			sub += newSeq.charAt(i);
-		}
-		String preans = "";
-		for (int i = sub.length() - 1; i >= 0; i--) {
-			preans += sub.charAt(i);
-		}
-		ReversedCharSequence ans = new ReversedCharSequence(preans);
-		return ans;
-
+    public CharSequence subSequence (int start, int end) {
+    		return output.subSequence(start,end);
     }
 
     public String toString () {
-    		return newSeq;
+    		return output.toString();
     }
     
-    public static void main(String[] args){
-    		ReversedCharSequence ans = new ReversedCharSequence("PeterJin");
-    		for (int i = 0; i < ans.length(); i++){
-    			System.out.println(ans.charAt(i));
-    		}
-    			       
-    	System.out.println(ans);
-    	
-    }
 }
 	    
